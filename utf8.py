5a5726536f88ec002be51350f84c78ba09ca0dab
FarOutWest <westDcastro@gmail.com>
2017-08-28 11:10:21 -0600

    added text file input and have it mostly working for some reason I get a Traceback error call on the last char in the file because 'print(unicodedata.name(c)) has a ValueError: no such name even though it works no problem for the first 18 chars

--- utf8.py
+++ utf8.py
@@ -2,8 +2,25 @@
 import sys
 import unicodedata
 
-u = chr(233) + chr(0x0bf2) + chr(3972) + chr(6000) + chr(13231)
+print('-------------------------------------------------------------')
+print("Unicodedata Decryption")
+print('-------------------------------------------------------------')
+print('')
 
-for i, c in enumerate(u):
-    print(i, '%04x' % ord(c), unicodedata.category(c), end=" ")
+chars = open('chars.txt', 'r')
+w = chars.read()
+print('chars.txt: ', w)
+chars.close()
+
+#u = chr(233) + chr(0x0bf2) + chr(3972) + chr(6000) + chr(13231)
+#print('u: ', u)
+
+print('-------------------------------------------------------------')
+print('')
+
+for i, c in enumerate(w):   #(chars.read()): #(u):
+    print(i, '%04x' % ord(c), unicodedata.category(c), end = " ")
     print(unicodedata.name(c))
+
+print('')
+print('-------------------------------------------------------------')
