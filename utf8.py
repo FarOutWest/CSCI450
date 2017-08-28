f10e08b0d20dcaed6e3e1384f61299f057c311df
FarOutWest <westDcastro@gmail.com>
2017-08-28 10:40:20 -0600

    expiremented with python unicodedata extension, have a basic form of assignment. Need to add the ability to parse a file with many unicode chars

---
+++ utf8.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+import sys
+import unicodedata
+
+u = chr(233) + chr(0x0bf2) + chr(3972) + chr(6000) + chr(13231)
+
+for i, c in enumerate(u):
+    print(i, '%04x' % ord(c), unicodedata.category(c), end=" ")
+    print(unicodedata.name(c))
