cf9bcc6d4fc85cf1bd80d45a923cd8c1f6e7ccd1
FarOutWest <westDcastro@gmail.com>
2017-08-28 11:16:36 -0600

    figured out that my issue with read was a LF (Line Feed) char at the end of my text file from the import into the python script. I updated the str of input to disclude the final LF char and it now works without a hitch

--- utf8.py
+++ utf8.py
@@ -2,25 +2,22 @@
 import sys
 import unicodedata
 
+print('')
 print('-------------------------------------------------------------')
 print("Unicodedata Decryption")
 print('-------------------------------------------------------------')
-print('')
 
 chars = open('chars.txt', 'r')
 w = chars.read()
+w = w[:-1]
 print('chars.txt: ', w)
 chars.close()
 
-#u = chr(233) + chr(0x0bf2) + chr(3972) + chr(6000) + chr(13231)
-#print('u: ', u)
-
 print('-------------------------------------------------------------')
-print('')
 
 for i, c in enumerate(w):   #(chars.read()): #(u):
     print(i, '%04x' % ord(c), unicodedata.category(c), end = " ")
     print(unicodedata.name(c))
 
-print('')
 print('-------------------------------------------------------------')
+print('')
